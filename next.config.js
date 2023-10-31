d40abe98fcb184331f97a9ec0a995366cd606041
Wanda Aizul Fahmi <56492749+waaizulfahmi@users.noreply.github.com>
2023-10-31 17:43:37 +0700

    Update next.config.js
    update next config js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,14 @@
 /** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa')({
+//     dest: 'public',
+//     customWorkerDir: 'serviceworker',
+//     // register: true,
+//     // skipWaiting: false,
+//     // swSrc: '/sw2.js',
+//     // importScripts: ['/sw2.js'],
+// });
 
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    sw: 'sw.js',
-});
-
-// const nextConfig = {
-//     images: {
-//         domains: ['res.cloudinary.com', 'placehold.jp', 'i.imgur.com', 'imgur.com', 'nurz.site'],
-//     },
-//     eslint: {
-//         dirs: ['app', 'utils', 'components', 'redux'], // Only run ESLint on the [...] directories during production builds (next build)
-//     },
-//     reactStrictMode: false,
-// };
-
-const nextConfig = withPWA({
+const nextConfig = {
     images: {
         domains: ['res.cloudinary.com', 'placehold.jp', 'i.imgur.com', 'imgur.com', 'nurz.site'],
     },
@@ -28,21 +19,34 @@ const nextConfig = withPWA({
     experimental: {
         newNextLinkBehavior: true,
     },
-    // Customize the HTML head element
-    // async headers() {
-    //     return [
-    //         {
-    //             source: '/(.*)',
-    //             headers: [
-    //                 {
-    //                     key: 'Content-Security-Policy',
-    //                     value: 'upgrade-insecure-requests',
-    //                 },
-    //             ],
-    //         },
-    //     ];
-    // },
-});
+};
+
+// const nextConfig = withPWA({
+//     images: {
+//         domains: ['res.cloudinary.com', 'placehold.jp', 'i.imgur.com', 'imgur.com', 'nurz.site'],
+//     },
+//     eslint: {
+//         dirs: ['app', 'utils', 'components', 'redux'], // Only run ESLint on the [...] directories during production builds (next build)
+//     },
+//     reactStrictMode: false,
+//     experimental: {
+//         newNextLinkBehavior: true,
+//     },
+//     // Customize the HTML head element
+//     // async headers() {
+//     //     return [
+//     //         {
+//     //             source: '/(.*)',
+//     //             headers: [
+//     //                 {
+//     //                     key: 'Content-Security-Policy',
+//     //                     value: 'upgrade-insecure-requests',
+//     //                 },
+//     //             ],
+//     //         },
+//     //     ];
+//     // },
+// });
 
 // module.exports = process.env.NODE_ENV === 'development' ? nextConfig : nextConfigWithPWA;
 module.exports = nextConfig;
