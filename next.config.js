07aa4ccd102ddcfaa1c258991cadc8b5db1e056d
waaizulfahmi <wandaaizulf@gmail.com>
2023-11-12 10:31:41 +0700

    edit next config js link image

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@
 
 const nextConfig = {
     images: {
-        domains: ['res.cloudinary.com', 'placehold.jp', 'i.imgur.com', 'imgur.com', 'nurz.site'],
+        domains: ['res.cloudinary.com', 'placehold.jp', 'i.imgur.com', 'imgur.com', 'be.gmooc4d.id'],
     },
     eslint: {
         dirs: ['app', 'utils', 'components', 'redux'], // Only run ESLint on the [...] directories during production builds (next build)
